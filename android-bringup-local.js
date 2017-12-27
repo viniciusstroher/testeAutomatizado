109a7c34c7843ef6effc81af91c3e0a3d603a21e
Vini <viniciusferreirawk@gmail.com>
2017-12-27 21:51:24 -0200

    firt

--- android-bringup-local.js
+++ android-bringup-local.js
@@ -21,7 +21,18 @@ describe("Bringup TEST", function () {
      //  _.clone(require("./helpers/caps").android19);
     
     //USA ANDROID - celular fisico - 6.0.1 - J5 metal
-    var desired = _.clone(require("./helpers/caps").android19);
+    var desired = _.clone(
+                    {
+                      browserName: '',
+                      'appium-version': '1.6',
+                      platformName: 'Android',
+                      platformVersion: '6.0.1',
+                      deviceName: 'Android Emulator',
+                      app: 'br.com.bringupapp', // will be set later
+                      noReset: true,
+                      fullReset: false
+                    }
+    );
     
     //APP A SER INSTALADO
     //var androidApiDemos = "https://build.phonegap.com/apps/1970105/download/android";
@@ -56,11 +67,31 @@ describe("Bringup TEST", function () {
   });
 
   
-  it("should find an element", function () {
+  it("login", function () {
     return driver
-      .elementByAccessibilityId('Graphics')
-      .click()
-      .elementByAccessibilityId('Arcs')
+        .contexts().then(function (contexts) { // get list of available views. Returns array: ["NATIVE_APP","WEBVIEW_1"]
+            console.log('contextos',contexts)
+            return driver.context(contexts[1]); // choose the webview context
+        })
+      let el1 = driver.element("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.view.View[3]/android.view.View/android.view.View[2]/android.view.View[1]/android.widget.Button");
+      el1.click();
+      let el2 = driver.element("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View[3]/android.view.View/android.view.View[2]/android.view.View/android.view.View/android.view.View[2]/android.widget.EditText");
+      el2.click();
+      let el3 = driver.element("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout");
+      el3.setValue("viniciusferreirawk@gmail.com");
+      let el4 = driver.element("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View[3]/android.view.View/android.view.View[2]/android.view.View/android.view.View/android.view.View[3]");
+      el4.click();
+      el4.setValue("gree");
+      let el5 = driver.element("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View[3]/android.view.View/android.view.View[2]/android.view.View/android.view.View/android.view.View[2]/android.widget.EditText");
+      el5.click();
+      el5.click();
+      el5.setValue("viniciusferreirawk@gmail.com");
+      let el6 = driver.element("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View[3]/android.view.View/android.view.View[2]/android.view.View/android.view.View/android.widget.Button[1]");
+      el6.click();
+
+
+      driver.sleep(20000);
+      /*.elementByAccessibilityId('Arcs')
         .should.eventually.exist
       .back()
       .elementByName('App')
@@ -70,7 +101,7 @@ describe("Bringup TEST", function () {
       .elementsByAndroidUIAutomator('new UiSelector().enabled(true)')
         .should.eventually.have.length.above(20)
       .elementByXPath('//android.widget.TextView[@text=\'API Demos\']')
-        .should.exists;
+        .should.exists;*/
   });
   
 
